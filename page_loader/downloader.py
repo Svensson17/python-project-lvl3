9056c265f534e5cfcc1bec0021b7e1aa04a997a2
Svennson17 <kiryl.shvedau@gmail.com>
2021-12-17 17:19:53 +0100

    step1 working version

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -1,18 +1,25 @@
 import requests
 from urllib.parse import urlparse
 import re
+from bs4 import BeautifulSoup
 
 
 def download(url, path):
-    result = requests.get(url)
-    html = result.text
+    request = requests.get(url)
+    response = request.text
     with open('url', 'w') as file:
-        file.write(html)
+        file.write(response)
     parced_url = urlparse(url)
     file_name = parced_url.netloc
     new_file_name = re.sub(r'([^a-zA-Z0-9])', '-', file_name)
     result = path + new_file_name + '.html'
     return result
+
+def download_page():
+    request = requests.get(url)
+    soup = BeautifulSoup(request, 'html.parser')
+
+
 
 
 
