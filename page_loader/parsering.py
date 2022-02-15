e51c3c0af6fa23f1a3ab9d58632b3a86ab5620a0
Svennson17 <kiryl.shvedau@gmail.com>
2022-02-15 19:02:23 +0100

    step 6

--- page_loader/parsering.py
+++ page_loader/parsering.py
@@ -4,5 +4,5 @@ import argparse
 def parse_data():
     parser = argparse.ArgumentParser(description='Page download')
     parser.add_argument('url')
-    parser.add_argument('path', dest='-o')
+    parser.add_argument('-o', dest='path')
     return parser.parse_args()
